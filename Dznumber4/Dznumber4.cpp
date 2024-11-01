c0dfe33359b025faa8293bedc15ab9a4d2f617b8
Бижова Андрей <andreybizhov@gmail.com>
2024-11-01 17:35:02 +0500

    Добавьте файлы проекта.

---
+++ Dznumber4/Dznumber4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    std::cout << "---------------------\n";
+    std::cout << "<Я коровка Мууууууу> \n";
+    std::cout << "---------------------\n";
+    std::cout << "\t\\  Л__Л\n";
+    std::cout << "\t \\ (00)\\_______\n";
+    std::cout << "\t   (__)\\       )\\/\\\n";
+    std::cout << "\t       ||-w---/\n";
+    std::cout << "\t       ||    ||";
+}
+
